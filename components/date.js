f11b13d35f05eedbf0e3f889592eae5a18306762
Mohammed Ali Agha <mtxshiftg@gmail.com>
2022-03-06 15:54:51 +0300

    changes: Make website more personalized

--- components/date.js
+++ components/date.js
@@ -1,6 +1,11 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format } from "date-fns";
+import { tr } from "date-fns/locale";
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  const date = parseISO(dateString);
+  return (
+    <time dateTime={dateString}>
+      {format(date, "EEEE, dd.MM.yyyy", { locale: tr })}
+    </time>
+  );
 }
