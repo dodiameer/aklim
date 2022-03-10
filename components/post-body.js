6f2e3c85ff2e8a5f5ba83c84c6465229ab597fba
Mohammed Ali Agha <mtxshiftg@gmail.com>
2022-03-10 14:40:28 +0300

    fix: Use Tailwind typography plugin

--- components/post-body.js
+++ components/post-body.js
@@ -1,10 +1,12 @@
-import markdownStyles from './markdown-styles.module.css'
-import { PortableText } from '@portabletext/react'
+import markdownStyles from "./markdown-styles.module.css";
+import { PortableText } from "@portabletext/react";
 
 export default function PostBody({ content }) {
   return (
-    <div className={`max-w-2xl mx-auto ${markdownStyles.markdown}`}>
+    <div
+      className={`max-w-prose mx-auto prose lg:prose-lg ${markdownStyles.markdown}`}
+    >
       <PortableText value={content} />
     </div>
-  )
+  );
 }
